100fdc6ec1c1b3a50f6d6539545c9a75f3bf0133
Natzgun <itzlilouxx@gmail.com>
2025-02-17 21:41:01 -0500

    Now we can send a one message from client to server :D

--- client.c
+++ client.c
@@ -1,8 +1,13 @@
 #include "Utility.h"
+#include <stdio.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <unistd.h>
+
+#define BUF_SIZE 100
 
 int main(int argc, char *argv[]) {
+  char buff[BUF_SIZE];
   struct sockaddr_un addr;
   int sock_fd;
 
@@ -18,6 +23,12 @@ int main(int argc, char *argv[]) {
     errExit("connect");
 
   printf("FD of client in client: %d\n", sock_fd);
+
+  while (1) {
+    printf("client: ");
+    fgets(buff, BUF_SIZE, stdin);
+    write(sock_fd, buff, BUF_SIZE);
+  }
 
   return 0;
 }
--- server.c
+++ server.c
@@ -3,11 +3,13 @@
 #include <unistd.h>
 
 #define BACKLOG 1
+#define BUF_SIZE 100
 
 int main(int argc, char *argv[]) {
 
   struct sockaddr_un server_addr;
   int server_fd, client_fd;
+  char buff[BUF_SIZE];
 
   server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
   if (server_fd == -1)
@@ -31,6 +33,9 @@ int main(int argc, char *argv[]) {
       errExit("Socket Client");
 
     printf("FD of client in server: %d\n", client_fd);
+
+    read(client_fd, buff, BUF_SIZE);
+    printf("Client %d: %s", client_fd, buff);
 
     if (close(client_fd) == -1)
       errExit("close");
